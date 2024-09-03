15d3e994d589ec5e7809a10df9af900899c4a298
LoVrO312 <149537548+LoVrO312@users.noreply.github.com>
2024-09-03 15:59:59 +0200

    Add files via upload
    First commit - Web API

---
+++ Web API/Introduction.WebAPI/Controllers/SubjectController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Introduction.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SubjectController : Controller
+    {
+        static Department MathDept = new Department(134, "Math");
+
+        [HttpGet]
+        [Route("Read/{id}")]
+        public IActionResult Get(int id)
+        {
+            Subject? foundSubject = MathDept.FindSubject(id);
+            
+            if (foundSubject == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(foundSubject);
+        }
+
+        [HttpGet]
+        [Route("ReadAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(MathDept.Subjects);
+        }
+
+
+        [HttpPut]
+        [Route("Update/{id}")]
+        public IActionResult UpdateSubject([Required] int id,[Required] string newName)
+        {
+            if(MathDept.ChangeSubjectName(id, newName) == true)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public IActionResult CreateSubject(Subject newSubject)
+        {
+            if(MathDept.AddSubject(newSubject) == true)
+            {
+                return Ok();
+            }
+            return BadRequest("Unable to add subject");
+        }
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public IActionResult RemoveSubject(int id)
+        {
+            if(MathDept.RemoveSubject(id) == true)
+            {
+                return Ok();
+            }
+            
+            return BadRequest("Can't delete object");
+        }
+
+    }
+}
---
+++ Web API/Introduction.WebAPI/Subject.cs
@@ -0,0 +1,18 @@
+﻿namespace Introduction.WebAPI
+{
+    public class Subject
+    {
+        private static int _IdCounter = 1;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime TimeCreated { get; set; }
+
+        public Subject (string name)
+        {
+            Id = _IdCounter++;
+            Name = name;
+            TimeCreated = DateTime.Now;
+        }
+    }
+}
